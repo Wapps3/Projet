b1ee90b0fe66d8a08f2c51c07ca02ecf76c4302f
Duvignau Thomas <thomaslaforce@yahoo.fr>
2018-11-15 11:10:40 +0100

    Update Lemmatisation_Dico.py
    update lemm

--- Lemmatisation_Dico.py
+++ Lemmatisation_Dico.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import Initialize_Dico
 from nltk.tokenize import word_tokenize
 
 
@@ -13,8 +12,7 @@ def Lemmatisation(liste,dico):
         texte_lemmatized = ""
         
         texte_tokenized = word_tokenize(liste[nliste], language ='french')
-        
-        print(texte_tokenized)
+      
         
         for nmot in range( len(texte_tokenized) ):
             texte_lemmatized = texte_lemmatized + str(dico.get(texte_tokenized[nmot],texte_tokenized[nmot])) + " "
