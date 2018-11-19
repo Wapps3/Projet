31309d224c2b0ebfcdfceb34061914c9e5cb956a
clopatre <33488142+clopatre@users.noreply.github.com>
2018-11-19 12:10:23 +0100

    Add files via upload

---
+++ retrait_Lieu_Perso.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 19 12:07:22 2018
+
+@author: bmchl
+"""
+
+#Besoin des packages :
+from nltk.tokenize import word_tokenize #pour tokeniser le text par mot
+# + la liste txt des stop word (4 txt)
+#importer import_stopword et retrait_stopword
+import re
+#pour les expressions regulieres
+
+
+#On va retirer les personnages et les lieux du textes en les plaçant dans un dictionnaire
+#triés selon leurs parties
+
+def retrait_Lieu_Perso(texte):
+    sw=import_stopword()
+    texte=retrait_stopword(sw[0],texte)
+    texte=retrait_stopword(sw[1],texte)
+    dicoPerso = dict()
+    taille = len(texte)
+    for i in range (0,taille):
+        perso = list()
+        token_bel_ami = word_tokenize(texte[i], language ='french')
+        taille2=len(token_bel_ami)
+        j=0
+        while (j!=taille2):
+            if(token_bel_ami[j]=="rue" or token_bel_ami[j]=="impasse" or token_bel_ami[j]=="avenue" or 
+               token_bel_ami[j]=="boulevard" or token_bel_ami[j]=="gare"):
+                perso.append(token_bel_ami[j])
+            elif(re.match('[A-Z]+|É',token_bel_ami[j])):
+                    perso.append(token_bel_ami[j])
+            j=j+1
+        dicoPerso[i]=perso
+    return dicoPerso
