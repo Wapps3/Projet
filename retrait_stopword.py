261f3183a0c8272d0b8b9ce73fda6be09bcad714
Emerois <44430936+Emerois@users.noreply.github.com>
2018-11-20 17:08:55 +0100

    Update retrait_stopword.py

--- retrait_stopword.py
+++ retrait_stopword.py
@@ -1,21 +1,43 @@
 #--------------------------------FONCTION retrait stopword
 #Besoin des packages :
-from nltk.tokenize import word_tokenize #pour tokeniser le text par mot
-# + avoir lancé la fonction import_stopword
+#from nltk.tokenize import word_tokenize #pour tokeniser le text par mot 
+# + Avoir la liste des stopword et eventuellement changer le chemin dans la fonction
 
-#la liste en entrée correspond à la liste des stopword obtenue après avoir alncé la fonction import
-#stop_word[0] = classic, 1 = lemmatisé, 2 = stemmisé, 3 = ponctuation 
-def retrait_stopword(liste, text):
+# text --> liste de text
+# stopword -->  1 = stop_word classic, 2 = stop_word lemmatisé, 3 = stop_word stemmisé, 4 = stop_word ponctuation 
+def retrait_stopword(text, stopword):
     final = []
     sep = ' '
-    for nText in range(len(text)):
-        text_sans_sw=[]
-        text_token = word_tokenize(text[nText], language ='french')
-        for word in text_token:
-            if word not in liste:
-                text_sans_sw.append(word)
+    #Import des listes de stopword en fonction du choix de l'utilisateur
+    if stopword == 1 :
+        #Import des sw classiques
+        sw = open('stop_word_classic.txt', 'r')
+        stop_word = sw.read()
+        stop_word = word_tokenize(stop_word, language ='french')
+    elif stopword == 2:
+        #Import des sw lemmatisés
+        sw = open('stop_word_lem.txt', 'r')
+        stop_word = sw.read()
+        stop_word = word_tokenize(stop_word, language ='french') 
+    elif stopword == 3:
+        #Import des sw stemmatisés / racinisés
+        sw = open('stop_word_stem.txt', 'r')
+        stop_word = sw.read()
+        stop_word = word_tokenize(stop_word, language ='french')
+    elif stopword == 4:
+        #Import de la ponctuation
+        sw = open('stop_word_ponctuation.txt', 'r')
+        stop_word = sw.read()
+        stop_word = word_tokenize(stop_word, language ='french')
+    #Si la liste choisie est entre 1 et 4 c'est correct donc on retire, sinon afficher erreur
+    if 0 < stopword < 5:
+        for nText in range(len(text)):
+            text_sans_sw=[]
+            text_token = word_tokenize(text[nText], language ='french')
+            for word in text_token:
+                if word not in stop_word:
+                    text_sans_sw.append(word)
         final.append(sep.join(text_sans_sw))
+    else :
+        print("Veuillez saisir une liste de stopword valide (1, 2, 3 ou 4 )")
     return final
-
-retrait_stopword(sw[0], l_bel_ami)
-#sw[0] = liste 1 (stop_word classic) obtenue lors de l'import des listes
