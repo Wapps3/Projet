82081bdd093c02ffea2b512a1ff0a28e3d991d91
clopatre <33488142+clopatre@users.noreply.github.com>
2018-11-22 08:40:58 +0100

    Update retrait_Lieu_Perso.py

--- retrait_Lieu_Perso.py
+++ retrait_Lieu_Perso.py
@@ -4,11 +4,16 @@ Created on Mon Nov 19 12:07:22 2018
 
 @author: bmchl
 """
+import os
+os.chdir('C:/Users/bmchl/Desktop/Cours/Projet/Projet-Pr-Traitement-master')
+#----------------------ATTENTION-----------------------
+#REMPLACER LE CHEMIN CI DESSUS PAR LE CHEMIN OU SE TROUVE LE FICHIER RETRAIT_STOPWORD
+
 
 #Besoin des packages :
 from nltk.tokenize import word_tokenize #pour tokeniser le text par mot
 # + la liste txt des stop word (4 txt)
-#importer import_stopword et retrait_stopword
+from retrait_stopword import retrait_stopword
 import re
 #pour les expressions regulieres
 
@@ -17,23 +22,23 @@ import re
 #triés selon leurs parties
 
 def retrait_Lieu_Perso(texte):
-    texte=retrait_stopword(texte,1)
+    texte=retrait_stopword(texte,1)     #retire les stopword
     texte=retrait_stopword(texte,2)
-    dicoPerso = dict()
+    dicoPerso = dict()          #On créé un dico ou on va mettre les valeurs
     taille = len(texte)
-    for i in range (0,taille):
-        perso = list()
-        token_bel_ami = word_tokenize(texte[i], language ='french')
+    for i in range (0,taille):      #pour chaque partie du texte
+        perso = list()              #On va mettre les differents perso et lieux de chaque partie
+        token_bel_ami = word_tokenize(texte[i], language ='french')     #on tokenise pour avoir qu'une liste de mot
         taille2=len(token_bel_ami)
         j=0
-        while (j!=taille2):
+        while (j!=taille2):     #Pour chaque mot
             if(token_bel_ami[j]=="rue" or token_bel_ami[j]=="impasse" or token_bel_ami[j]=="avenue" or 
-               token_bel_ami[j]=="boulevard" or token_bel_ami[j]=="gare"):
-                perso.append(token_bel_ami[j])
-            elif(re.match('[A-Z]+|É',token_bel_ami[j])):
+               token_bel_ami[j]=="boulevard" or token_bel_ami[j]=="gare"):  #Indique que c'est un lieu
+                perso.append(token_bel_ami[j])      #Si oui on l'ajoute à la liste
+            elif(re.match('[A-Z]+|É',token_bel_ami[j])):    #Si le mot commence par une majuscule (nom ou ville ou erreur des fois)
                     perso.append(token_bel_ami[j])
             j=j+1
-        if(len(perso)>0):
+        if(len(perso)>0):   #Si la liste n'est pas nulle on ajoute au dico qui affiche le numero de partie et la liste
             dicoPerso[i]=perso
         
     return dicoPerso
