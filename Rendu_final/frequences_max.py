35d64b422e6752ffbf38f46fe6c26675f765a790
CamilleDes <45229065+CamilleDes@users.noreply.github.com>
2018-11-28 11:30:01 +0100

    Add files via upload

---
+++ Rendu_final/frequences_max.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
+
+def frequence_max(texte_l,n):
+    #texte_l est une liste de texte
+    #n est le nombre de mots que l'utilisateur souhaite afficher
+    texte_c=' '.join(texte_l)
+    token=word_tokenize(texte_c, language ='french')#Tokenise le texte
+    freq=FreqDist(token)#Fréquence de chaque token dans la liste
+    res=freq.most_common(n)#Affiche les n token les plus fréquents dans le texte
+    return res
+
+#import os
+#os.chdir('E:/Cours/Projet')
+
+fichier=open("Bel_Ami.txt","r")
+t=fichier.read()
+fichier.close
+
+texte=[t]
+freq_max=frequence_max(texte,10)
+#print(freq_max)
