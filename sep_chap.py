cf6a4e4bc23ab43e26d25e509d79f038b79215c7
CamilleDes <45229065+CamilleDes@users.noreply.github.com>
2018-11-28 10:48:36 +0100

    Add files via upload

---
+++ sep_chap.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+def sep_chap(text_l):
+    Chap=["\nI\n","\nII\n","\nIII\n","\nIV\n","\nV\n","\nVI\n","\nVII\n","\nVIII\n","\nIX\n","\nX\n"]#Liste des numéros de chapitres de Bel Ami
+    text_c=text_l[0]
+    for ch in Chap:
+        text_c=text_c.replace(ch,'*')
+    print("Nombre de chapitres : ",text_c.count("*"))#18 occurences qui remplace les 18 chapitres
+    list_chap=text_c.split("*")[1:len(text_c.split("*"))]#On ignore le début du fichier qui ne correspond pas à un chapitre
+    return (list_chap)
