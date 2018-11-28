a66af06b55a4f6adfb07f67aaf0f76f7d576f033
CamilleDes <45229065+CamilleDes@users.noreply.github.com>
2018-11-28 10:49:15 +0100

    Delete segmentation_chap.py

--- segmentation_chap.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-def sep_chap(text):
-    Chap=["\nI\n","\nII\n","\nIII\n","\nIV\n","\nV\n","\nVI\n","\nVII\n","\nVIII\n","\nIX\n","\nX\n"]
-    for i in Chap:
-        text=text.replace(i,'*')
-    print("Nombre de chapitres : ",text.count("*"))#18 occurences qui remplace les 18 chapitres
-    list_chap=text.split("*")[1:len(text.split("*"))]
-    return (list_chap)
